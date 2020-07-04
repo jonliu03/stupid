a4db5d17766152daacc31a444f13ebf43e692fe9
Jon Liu <chunleiliu@Winstons-Mac.local>
2020-07-04 13:00:19 -0400

    Added env

--- env/app.py
+++ env/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
-@app.route("/Joyce")
-def Joyce():
-    return "You are stupid"
+@app.route("/hello", methods=["POST"])
+def hello():
+    name = request.form.get("name")
+    return render_template("hello.html", name=name)
